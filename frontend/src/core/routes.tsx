fa8384bdc4b750f202a9c926e89abd64634d99b9
KengniJohan <kengnijordan9@gmail.com>
2025-12-01 16:40:44 +0100

    Initier l'implémentation de l'UI de listing des sinisstres

--- frontend/src/core/routes.tsx
+++ frontend/src/core/routes.tsx
@@ -3,6 +3,7 @@ import AccordsDevis from "../feature/gestion_devis/page/AccordsDevis"
 import { createBrowserRouter } from "react-router-dom";
 import AjouterSinistre from "../feature/gestion_sinistre/ajouter_sinistre/pages/ajouter_sinistre";
 import AjoutSinistreResultat from "../feature/gestion_sinistre/ajouter_sinistre/pages/ajout_sinistre_resultat";
+import ListingSinistres from "../feature/gestion_sinistre/listing_sinistre/listing_sinistres";
 
 export const routes = [
     {
@@ -16,6 +17,10 @@ export const routes = [
             {
                 path: "sinistre/ajout_sinistre_resultat",
                 element: <AjoutSinistreResultat/>
+            },
+            {
+                path: "sinistre",
+                element: <ListingSinistres/>
             },
             {
                 path: "/",
